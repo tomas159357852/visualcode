e1882f10b999d7ec7d9da1ce95435ea6b79d954b
tomas159357852 <george159357852@gmail.com>
2021-05-12 14:21:28 -0500

    Actualizando

--- erepetitive.java
+++ erepetitive.java
@@ -17,8 +17,39 @@ public class erepetitive {
         System.out.println("La suma de los 10 numeros es: "+sumanumeros);
     }
 
+
+public static void suma10numerosm() {
+    //Declarar variables
+    int contador=1;
+    double numeros, sumanumeros=0;
+    //datos de entrada y proceso
+    do{ 
+    System.out.println("Ingrese el valor del contador "+contador+":");
+    numeros=teclado.nextDouble();
+    sumanumeros=sumanumeros+numeros;
+    contador++;
+}
+    while (contador<=10);
+    System.out.println("La suma de los 10 numeros es: "+sumanumeros);
+}
+
+public static void suma10numerosma() {
+    //Declarar variables
+    
+    double numeros, sumanumeros=0;
+    //datos de entrada y proceso
+    for (int contador=1; contador<=10; contador++) { 
+    System.out.println("Ingrese el valor del contador "+contador+":");
+    numeros=teclado.nextDouble();
+    sumanumeros=sumanumeros+numeros;
+}
+    System.out.println("La suma de los 10 numeros es: "+sumanumeros);
+}
+
     public static void main(String[] args) {
-        System.out.println("Buenas a todos!");
-        suma10numeros();
-    }
+    System.out.println("Buenas a todos!");
+    //suma10numeros();
+    //suma10numerosm();
+    suma10numerosma();
+}
 }
